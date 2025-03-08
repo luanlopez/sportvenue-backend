a1426a609816bae967d50527fede3dbc0306f9cb
˜Luan <luannlopessilva@outlook.com>
2025-03-08 10:58:52 -0300

    fix: test new deployment

--- src/common/logger/loki-logger.service.ts
+++ src/common/logger/loki-logger.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
-const SERVICE_NAME = 'sportmap-backend';
+const SERVICE_NAME = 'sportmap-backend-production';
 
 interface LokiLogLabels {
   Language: string;
